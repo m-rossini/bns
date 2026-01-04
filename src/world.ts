d778ec5466c15bf2693ed64bfbd27d4a41333590
Marcos Rossini <marcos.tengelmann@gmail.com>
2026-01-04 10:48:47 +0000

    refactor: move world instance creation to worldWindow, add Dimensions type

---
+++ src/world.ts
@@ -0,0 +1,30 @@
+// World singleton: holds simulation state and immutable config
+import { worldConfig, Dimensions } from './config';
+
+export interface WorldState {
+  // Example state: add more as needed
+  tick: number;
+  totalTime: number;
+  timer?: number;
+}
+
+export class World {
+  public readonly config = worldConfig;
+  public state: WorldState;
+  private dimensions: any;
+  
+  constructor(dimensions?: Dimensions) {
+    this.state = {
+      tick: 0,
+      totalTime: 0
+    };
+    this.dimensions = dimensions;
+    console.log('>>> dimensions', dimensions);
+  }
+  step(time: number, delta: number) {
+    this.state.tick += delta;
+    this.state.totalTime += delta;
+    this.state.timer = time;
+    return this.state;
+  }
+}
